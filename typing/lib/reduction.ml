ccc52aceba777c1fff1b154f86e8c0d45637d10f
seelx3 <seelx3@gmail.com>
2024-04-20 17:58:38 +0900

    refactor: nomilize

--- typing/lib/reduction.ml
+++ typing/lib/reduction.ml
@@ -1,19 +1,17 @@
 (* TODO: define a function `reduce` which performs exactly single step reduction *)
 
+exception Nomilization_error
+
 let rec normalize (e : Syntax.prog) : Syntax.prog =
   match e with
   | Syntax.Add (n1, n2) -> (
     match (normalize n1, normalize n2) with
     | Syntax.Int n1, Syntax.Int n2 -> Syntax.Int (n1 + n2)
-    | Syntax.Int n1, _ -> Syntax.Add (Syntax.Int n1, normalize n2)
-    | _, Syntax.Int n2 -> Syntax.Add (normalize n1, Syntax.Int n2)
-    | _, _ -> Syntax.Add (normalize n1, normalize n2))
+    | _, _ -> raise Nomilization_error)
   | Syntax.Lt (n1, n2) -> (
     match (normalize n1, normalize n2) with
     | Syntax.Int n1, Syntax.Int n2 -> Syntax.Bool (n1 < n2)
-    | Syntax.Int n1, _ -> Syntax.Lt (Syntax.Int n1, normalize n2)
-    | _, Syntax.Int n2 -> Syntax.Lt (normalize n1, Syntax.Int n2)
-    | _, _ -> Syntax.Lt (normalize n1, normalize n2))
+    | _, _ -> raise Nomilization_error)
   | Syntax.If (b, e1, e2) -> (
     match b with
     | Syntax.Bool b -> (
