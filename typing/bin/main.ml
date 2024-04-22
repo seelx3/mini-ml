8332052fa2f3dd472d3002da55b08ff5f42f9828
seelx3 <yuuhashi.26@gmail.com>
2024-04-22 19:37:21 +0900

    feat: add string_of_prog

--- typing/bin/main.ml
+++ typing/bin/main.ml
@@ -4,4 +4,9 @@ let () =
     then "Typable"
     else "Not typable"
   in
-  print_endline isTypable
+  print_endline isTypable;
+  let result =
+    Typing.Reduction.normalize
+      (Typing.Parse.parse "if (1 + 2) < 3 then 4 else 5")
+  in
+  print_endline (Typing.Syntax.string_of_prog result)
--- typing/lib/syntax.ml
+++ typing/lib/syntax.ml
@@ -8,3 +8,10 @@ type prog =
   | Product of prog * prog
   | Fst of prog
   | Snd of prog
+
+let rec string_of_prog = function
+  | Bool b -> string_of_bool b
+  | Int i -> string_of_int i
+  | Unit -> "()"
+  | Product (e1, e2) -> "(" ^ string_of_prog e1 ^ ", " ^ string_of_prog e2 ^ ")"
+  | _ -> failwith "Not implemented"
