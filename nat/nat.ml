48df339741a9c96bba0b4b094c7a143b2869d060
seelx3 <yuuhashi.26@gmail.com>
2024-04-22 19:25:32 +0900

    style: format nat.ml

--- nat/nat.ml
+++ nat/nat.ml
@@ -1,24 +1,17 @@
 type nut = Zero | Succ of nut
 
 let add n m =
-  let rec add' n m =
-    match n with
-    | Zero -> m
-    | Succ n' -> Succ (add' n' m)
-  in
-  add' n m;;
+  let rec add' n m = match n with Zero -> m | Succ n' -> Succ (add' n' m) in
+  add' n m
 
 let rec even n =
-  match n with
-  | Zero -> true
-  | Succ Succ n' -> even n'
-  | Succ n' -> false;;
+  match n with Zero -> true | Succ (Succ n') -> even n' | Succ n' -> false
 
 let pred n =
   match Some n with
   | None -> None
   | Some Zero -> None
-  | Some (Succ n') -> Some n';;
+  | Some (Succ n') -> Some n'
 
 let half n =
   let rec half' n =
@@ -27,21 +20,19 @@ let half n =
     | Succ Zero -> Zero
     | Succ (Succ n') -> Succ (half' n')
   in
-  half' n;;
+  half' n
 
-let half_opt n =
-  if even n then
-    Some (half n)
-  else
-    None;;
+let half_opt n = if even n then Some (half n) else None
 
-
-add (Succ (Succ (Succ Zero))) (Succ (Succ Zero));;
-even (Succ (Succ (Succ Zero)));;
-even (Succ (Succ (Succ (Succ Zero))));;
-pred (Succ (Succ (Succ Zero)));;
-pred Zero;;
-half (Succ (Succ (Succ (Succ (Succ Zero)))));;
-half (Succ (Succ (Succ (Succ Zero))));;
-half_opt (Succ (Succ (Succ (Succ (Succ Zero)))));;
-half_opt (Succ (Succ (Succ (Succ Zero))));;
+(* check functions using the following commands *)
+(*
+   add (Succ (Succ (Succ Zero))) (Succ (Succ Zero));;
+   even (Succ (Succ (Succ Zero)));;
+   even (Succ (Succ (Succ (Succ Zero))));;
+   pred (Succ (Succ (Succ Zero)));;
+   pred Zero;;
+   half (Succ (Succ (Succ (Succ (Succ Zero)))));;
+   half (Succ (Succ (Succ (Succ Zero))));;
+   half_opt (Succ (Succ (Succ (Succ (Succ Zero)))));;
+   half_opt (Succ (Succ (Succ (Succ Zero))));;
+*)
