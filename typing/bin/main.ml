4444cb982e16e395c57293e71ad94d178cce9b8f
seelx3 <yuuhashi.26@gmail.com>
2024-04-23 13:20:48 +0900

    style: main.ml

--- typing/bin/main.ml
+++ typing/bin/main.ml
@@ -1,12 +1,13 @@
+open Typing.Parse
+open Typing.Infer
+open Typing.Reduction
+open Typing.Syntax
+
 let () =
   let isTypable =
-    if Typing.Infer.typable (Typing.Parse.parse "if (1 + 2) < 3 then 4 else 5")
-    then "Typable"
+    if typable (parse "if (1 + 2) < 3 then 4 else 5") then "Typable"
     else "Not typable"
   in
   print_endline isTypable;
-  let result =
-    Typing.Reduction.normalize
-      (Typing.Parse.parse "if (1 + 2) < 3 then 4 else 5")
-  in
-  print_endline (Typing.Syntax.string_of_prog result)
+  let result = normalize (parse "if (1 + 2) < 3 then 4 else 5") in
+  print_endline (string_of_prog result)
