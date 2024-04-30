2ea43791faf04a2e228349bce5bb7c83721d31bb
seelx3 <seelx3@gmail.com>
2024-04-30 21:28:55 +0900

    feat: repl

--- bin/main.ml
+++ bin/main.ml
@@ -4,10 +4,26 @@ open Typing.Reduction
 open Typing.Syntax
 
 let () =
-  let isTypable =
-    if typable (parse "if (1 + 2) < 3 then 4 else 5") then "Typable"
-    else "Not typable"
-  in
-  print_endline isTypable;
-  let result = normalize (parse "if (1 + 2) < 3 then 4 else 5") in
-  print_endline (string_of_prog result)
+  if Array.length Sys.argv = 1 then (
+    Printf.printf "        MiniML version 0.0.1";
+    while true do
+      Printf.printf "\n# ";
+      try
+        let line = read_line () in
+        (* TODO: refactor this line *)
+        let line = String.sub line 0 ((String.length line) - 2) in
+        let command = parse line in
+        let isTypable = typable command in
+        match isTypable with
+        | false -> print_endline "Error: TypeError\n"
+        | true ->
+          let result = normalize command in
+          print_endline (string_of_prog result)
+      with End_of_file -> exit 0
+    done)
+  else
+    let arg1 = Sys.argv.(1) in
+    match arg1 with
+    | "--help" -> Printf.printf "Usage: ./typing [file]\n"
+    (* TODO: exec file *)
+    | _ -> Printf.printf "file: %s\n" arg1
