623ddcee01cdb4a68e6eb0f99e7792153b811c50
seelx3 <yuuhashi.26@gmail.com>
2024-04-30 17:49:11 +0900

    refactor

--- lib/reduction.ml
+++ lib/reduction.ml
@@ -1,66 +1,67 @@
-type env = (string * Syntax.prog) list
+open Syntax
 
-let rec reduce (e : Syntax.prog) (env : env) : (Syntax.prog * env) option =
+type env = (string * prog) list
+
+let rec reduce (e : prog) (env : env) : (prog * env) option =
   match (e, env) with
-  | Syntax.Var x, _ -> (
+  | Var x, _ -> (
     match List.assoc_opt x env with Some e' -> Some (e', env) | None -> None)
-  | Syntax.Add (n1, n2), _ -> (
+  | Add (n1, n2), _ -> (
     match (n1, n2) with
-    | Syntax.Int n1, Syntax.Int n2 -> Some (Syntax.Int (n1 + n2), env)
+    | Int n1, Int n2 -> Some (Int (n1 + n2), env)
     | _, _ -> (
       match reduce n1 env with
-      | Some (n1', _) -> Some (Syntax.Add (n1', n2), env)
+      | Some (n1', _) -> Some (Add (n1', n2), env)
       | None -> (
         match reduce n2 env with
-        | Some (n2', _) -> Some (Syntax.Add (n1, n2'), env)
+        | Some (n2', _) -> Some (Add (n1, n2'), env)
         | None -> None)))
-  | Syntax.Lt (n1, n2), env -> (
+  | Lt (n1, n2), env -> (
     match (n1, n2) with
-    | Syntax.Int n1, Syntax.Int n2 -> Some (Syntax.Bool (n1 < n2), env)
+    | Int n1, Int n2 -> Some (Bool (n1 < n2), env)
     | _, _ -> (
       match reduce n1 env with
-      | Some (n1', _) -> Some (Syntax.Lt (n1', n2), env)
+      | Some (n1', _) -> Some (Lt (n1', n2), env)
       | None -> (
         match reduce n2 env with
-        | Some (n2', _) -> Some (Syntax.Lt (n1, n2'), env)
+        | Some (n2', _) -> Some (Lt (n1, n2'), env)
         | None -> None)))
-  | Syntax.If (b, e1, e2), env -> (
+  | If (b, e1, e2), env -> (
     match b with
-    | Syntax.Bool b -> (
+    | Bool b -> (
       match b with true -> Some (e1, env) | false -> Some (e2, env))
     | _ -> (
       match reduce b env with
-      | Some (b', _) -> Some (Syntax.If (b', e1, e2), env)
+      | Some (b', _) -> Some (If (b', e1, e2), env)
       | None -> None))
-  | Syntax.Product (n1, n2), env -> (
+  | Product (n1, n2), env -> (
     match (n1, n2) with
     | _, _ -> (
       match reduce n1 env with
-      | Some (n1', _) -> Some (Syntax.Product (n1', n2), env)
+      | Some (n1', _) -> Some (Product (n1', n2), env)
       | None -> (
         match reduce n2 env with
-        | Some (n2', _) -> Some (Syntax.Product (n1, n2'), env)
+        | Some (n2', _) -> Some (Product (n1, n2'), env)
         | None -> None)))
-  | Syntax.Fst n, env -> (
+  | Fst n, env -> (
     match n with
-    | Syntax.Product (n1, _) -> Some (n1, env)
+    | Product (n1, _) -> Some (n1, env)
     | _ -> (
       match reduce n env with
-      | Some (n', _) -> Some (Syntax.Fst n', env)
+      | Some (n', _) -> Some (Fst n', env)
       | None -> None))
-  | Syntax.Snd n, env -> (
+  | Snd n, env -> (
     match n with
-    | Syntax.Product (_, n2) -> Some (n2, env)
+    | Product (_, n2) -> Some (n2, env)
     | _ -> (
       match reduce n env with
-      | Some (n', _) -> Some (Syntax.Snd n', env)
+      | Some (n', _) -> Some (Snd n', env)
       | None -> None))
-  | Syntax.Let (x, e1, e2), env -> 
-    Some (e2, (x, e1) :: env)
+  | Let (x, e1, e2), env -> Some (e2, (x, e1) :: env)
   | _ -> None
 
-let normalize (e : Syntax.prog) : Syntax.prog =
-  let rec normalize' (e : Syntax.prog) (env : env) : Syntax.prog =
+let normalize (e : prog) : prog =
+  let rec normalize' (e : prog) (env : env) : prog =
     match reduce e env with Some (e', env') -> normalize' e' env' | None -> e
   in
   normalize' e []
